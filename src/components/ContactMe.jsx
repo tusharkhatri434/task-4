5a380056b870e4f94042b1978e754abec7823021
[tushar khatri] <tusharkhatri8193@gmail.com>
2024-01-16 16:10:59 +0530

    new

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { HiOutlineMail } from "react-icons/hi"; // for mail icon
+import { BiMap } from "react-icons/bi"; // for Map icon
 
+import { FiGithub } from "react-icons/fi"; // github icon
+import { AiOutlineLinkedin } from "react-icons/ai"; // LinkedIn icon
 const ContactMe = () => {
   return (
-    <div>ContactMe</div>
-  )
+    <>
+      <div data-testid="ContactMeID">
+        ContactMe
+      </div>
+      <div data-testid="FooterID">
+        footer
+        </div>
+    </>
+  );
 }
 
 export default ContactMe
